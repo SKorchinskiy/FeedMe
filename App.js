1d7e6e2b631330843c7bce33cb844effae310ac5
SKorchinskiy <ddphyk@gmail.com>
2024-02-06 22:15:51 +0200

    + add theming

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
+import { theme } from "./src/infrastructure/theme/index";
+import {
+  useFonts as useOswaldFonts,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import {
+  useFonts as useLatoFonts,
+  Lato_400Regular,
+} from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [oswaldLoaded] = useOswaldFonts({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLatoFonts({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <RestaurantsScreen />
-    </>
+    </ThemeProvider>
   );
 }
