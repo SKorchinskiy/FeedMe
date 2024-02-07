bd8f0b2dc53a0fd1a65f1c11bec9cefdf08a093b
SKorchinskiy <ddphyk@gmail.com>
2024-02-07 17:31:04 +0200

    + add project asset components

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+import ClosedSign from "./closed";
+import OpenSign from "./open";
+import StarSign from "./star";
+import MealSign from "./meal";
+
+export { ClosedSign, OpenSign, StarSign, MealSign };
