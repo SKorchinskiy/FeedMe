ff013b6c33632954e9c336642ae8e40a54432a75
SKorchinskiy <ddphyk@gmail.com>
2024-02-07 17:34:54 +0200

    - extract styles from searchbar component to separate file

--- src/features/restaurants/components/searchbar/searchbar.component.js
+++ src/features/restaurants/components/searchbar/searchbar.component.js
@@ -1,20 +1,6 @@
 import React from "react";
-import styled from "styled-components/native";
 import { StyleSheet } from "react-native";
-import { Searchbar as DefaultSearchBar } from "react-native-paper";
-
-const SearchBarContainer = styled.View`
-  display: flex;
-  flex: 0.07;
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const CustomSearchBar = styled(DefaultSearchBar)`
-  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.35);
-  height: ${(props) => props.theme.sizes[3]};
-  border-radius: ${(props) => props.theme.sizes[0]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
+import { CustomSearchBar, SearchBarContainer } from "./searchbar.styles";
 
 export default function Searchbar({ searchQuery, onSearchQueryChange }) {
   return (
