72de1d97ea6acb2c316f837e4c8fba8a2f469534
SKorchinskiy <ddphyk@gmail.com>
2024-02-10 22:43:53 +0200

    + add restaurants provider

---
+++ src/providers/restaurants/restaurants.provider.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { getRestaurantsData } from "../../services/restaurants.service";
+import { LocationContext } from "../location/location.provider";
+
+export const RestaurantContext = createContext([]);
+
+export function RestaurantsProvider({ children }) {
+  const currentLocation = useContext(LocationContext);
+
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    const fetchRestaurantsData = async ({ location }) => {
+      const restaurantData = await getRestaurantsData({ location });
+      setRestaurants(restaurantData);
+    };
+
+    fetchRestaurantsData(currentLocation);
+  }, [currentLocation]);
+
+  return (
+    <RestaurantContext.Provider value={restaurants}>
+      {children}
+    </RestaurantContext.Provider>
+  );
+}
