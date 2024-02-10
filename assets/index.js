c65d97a06349cbe4ab9af5861110ddbb5951f3ad
SKorchinskiy <ddphyk@gmail.com>
2024-02-10 22:40:38 +0200

    - remove meal asset

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,5 @@
 import ClosedSign from "./closed";
 import OpenSign from "./open";
 import StarSign from "./star";
-import MealSign from "./meal";
 
-export { ClosedSign, OpenSign, StarSign, MealSign };
+export { ClosedSign, OpenSign, StarSign };
