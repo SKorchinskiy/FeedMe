f9865f7eebd51eb09ad0e3a04bf5c08c4185dabc
SKorchinskiy <ddphyk@gmail.com>
2024-02-10 22:44:13 +0200

    + add location and restaurants providers to the app

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
-import { theme } from "./src/infrastructure/theme/index";
+
 import {
   useFonts as useOswaldFonts,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
+
 import {
   useFonts as useLatoFonts,
   Lato_400Regular,
 } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme/index";
+import { ThemeProvider } from "styled-components";
+
+import { LocationProvider } from "./src/providers/location/location.provider";
+import { RestaurantsProvider } from "./src/providers/restaurants/restaurants.provider";
+
+import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   const [oswaldLoaded] = useOswaldFonts({
@@ -24,8 +31,12 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <RestaurantsScreen />
-    </ThemeProvider>
+    <LocationProvider>
+      <RestaurantsProvider>
+        <ThemeProvider theme={theme}>
+          <RestaurantsScreen />
+        </ThemeProvider>
+      </RestaurantsProvider>
+    </LocationProvider>
   );
 }
