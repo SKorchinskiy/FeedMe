e77cbaaded035f86f8918358142c40a55a4eafed
SKorchinskiy <ddphyk@gmail.com>
2024-02-11 23:34:09 +0200

    + add restaurant details screen

---
+++ src/features/restaurants/components/restaurant-details/restaurant-details.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const RestaurantDetailsContainer = styled.View``;
+
+export default function RestaurantDetails({ restaurant }) {
+  return <RestaurantDetailsContainer></RestaurantDetailsContainer>;
+}
