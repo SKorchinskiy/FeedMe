d56c282ab10aadce6045a6b4038254df4c381da3
SKorchinskiy <ddphyk@gmail.com>
2024-02-12 18:14:20 +0200

    + add mocked menu to the restaurant's entity

---
+++ src/entities/restaurant/restaurant.mock.js
@@ -0,0 +1,43 @@
+export const DEFAULT_RESTAURANT_MENU = {
+  sections: [
+    {
+      id: 1,
+      title: "Breakfast",
+      icon_name: "bread-slice",
+      items: [{ name: "Eggs Benedict" }, { name: "Classic Breakfast" }],
+    },
+    {
+      id: 2,
+      title: "Lunch",
+      icon_name: "food-turkey",
+      items: [
+        { name: "Burger w/ Fries" },
+        { name: "Steak Sandwich" },
+        { name: "Mushroom Soup" },
+      ],
+    },
+
+    {
+      id: 3,
+      title: "Dinner",
+      icon_name: "food-croissant",
+      items: [
+        { name: "Spaghetti Bolognese" },
+        { name: "Veal Cutlet with Chicken Mushroom Rotini" },
+        { name: "Steak Frites" },
+      ],
+    },
+    {
+      id: 4,
+      title: "Drinks",
+      icon_name: "food-variant",
+      items: [
+        { name: "Coffee" },
+        { name: "Tea" },
+        { name: "Modelo" },
+        { name: "Coke" },
+        { name: "Fanta" },
+      ],
+    },
+  ],
+};
