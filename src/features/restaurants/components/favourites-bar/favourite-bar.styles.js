afd5f171a0d3e063141c3fa3ff534fe845856a42
SKorchinskiy <ddphyk@gmail.com>
2024-02-19 23:06:24 +0200

    + add favourites bar component to show favourite restaurants

---
+++ src/features/restaurants/components/favourites-bar/favourite-bar.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const FavouritesBarContainer = styled.View`
+  margin-top: 20px;
+  margin-left: 10px;
+  margin-right: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
